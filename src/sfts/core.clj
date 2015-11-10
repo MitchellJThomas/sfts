6ba7198fbbe1bff06a3f5ab09221a26f63ca2304
Mitchell Thomas <mthomas@tripwire.com>
2015-11-09 23:41:44 -0800

    Get the track deets

--- src/sfts/core.clj
+++ src/sfts/core.clj
@@ -44,27 +44,26 @@
   (clojure.java.io/copy i (clojure.java.io/file "stuff"))
 
   ;; Parse all show titles and show urls
-  (def ht (-> @(http/get "http://xray.fm/programs/searchingforthesound/page:3?url=shows%2Fsearchingforthesound")
+  (def ht (-> @(http/get "http://xray.fm/programs/searchingforthesound/page:9?url=shows%2Fsearchingforthesound")
               :body
               hic/parse
               hic/as-hickory))
   (def show-map (hics/select
-              (hics/descendant (hics/class :broadcast) (hics/class :title) (hics/tag :a)) ht))
+              (hics/descendant (hics/class :broadcast) (hics/class :title) (hics/attr :href #(.startsWith % "/broadcasts/"))) ht))
   (def shows (map (fn [s] [(first  (:content s)) (get-in s [:attrs :href])] ) show-map))
   shows
 
   ;; Parse a given show
-  (def sho (-> @(http/get "http://xray.fm/broadcasts/6910")
+  (def sho (-> @(http/get "http://xray.fm/broadcasts/525")
               :body
               hic/parse
               hic/as-hickory))
-  sho
-  
-  ;; Loop through the shows
-  ;; download the broadcast and look for:
-  ;; <span class="track-time">8:09pm</span>
-  ;; <span class="track-title">Woman Would you be the Mother of My Children</span>
-  ;; <span class="track-artist">Kennelmas</span>
-  ;; <span class="track-album">Folkstone Prism</span>
-  ;; <span class="track-label">Rockadelic</span>
-)
+  (def show-tracks (hics/select (hics/descendant (hics/class :tracks-container) (hics/class :track)) sho))
+  (def show-track-map  (for [t (->> show-tracks
+                                    (map (fn [{c :content}] c))
+                                    (map #(filter map? %)))]
+                         (clojure.string/join ", " (map (fn [{{track :class} :attrs con :content}] (format "%s: %s" (re-find #"\w+$" track) (first con))) t))))
+  show-track-map
+
+
+  )
